511697c48744ebf969f4e2d9cd9093c5f569356f
shgus1224 <shgus1224@naver.com>
2023-11-06 16:07:00 +0900

    feat: firebase auth 기능을 위한 provider, auth 파일 추가

---
+++ lib/provider/myProvider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class UserInfos with ChangeNotifier {
+  late String _id, _nickname, _type;
+  String get id => _id;
+  String get nickname => _nickname;
+  String get type => _type;
+
+  void addUserInfo(userInfos) {
+    _id = userInfos['username'];
+    _nickname = userInfos['nickname'];
+    _type = userInfos['type'];
+    notifyListeners();
+  }
+
+  void editUserInfo(userInfos) {
+    if (userInfos['username'] != null) {
+      _id = userInfos['username'];
+    }
+
+    if (userInfos['nickname'] != null &&
+        userInfos['nickname'] != ' ' &&
+        userInfos['nickname'] != '') {
+      _nickname = userInfos['nickname'];
+    }
+
+    if (userInfos['type'] != null) {
+      _type = userInfos['type'];
+    }
+
+    notifyListeners();
+  }
+}
