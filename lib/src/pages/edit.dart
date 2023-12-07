9315a789b0d3904314f9c4e86db119f51583f034
shgus1224 <shgus1224@naver.com>
2023-12-07 18:49:35 +0900

    fix: 정보수정 페이지 추가

---
+++ lib/src/pages/edit.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:manlivetoung/src/auth.dart';
+import 'package:manlivetoung/provider/myProvider.dart';
+import 'package:provider/provider.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _login() async {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    dynamic isSigned = await AuthManage().signIn(username, password);
+
+    if (context.mounted) {
+      if (isSigned == '성공') {
+        context.read<UserInfos>().addUserInfo(
+            {'username': username, 'nickname': '홍길동', 'type': '건성'});
+        Navigator.pushReplacementNamed(context, '/recommend');
+      } else {
+        print('로그인 실패');
+        print(isSigned);
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Alert'),
+              content: Text(isSigned),
+              actions: [
+                TextButton(
+                  child: Text('닫기'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+
+    // isSigned
+    //     ? Navigator.pushReplacementNamed(context, '/main')
+    //     : print('로그인 실패');
+  }
+
+  // _auth() {
+  //   // 사용자 인증정보 확인. 딜레이를 두어 확인
+  //   Future.delayed(const Duration(milliseconds: 100), () {
+  //     if (FirebaseAuth.instance.currentUser == null) {
+  //       print('로그인');
+  //     }
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/logo.png',
+              height: 130,
+              width: 450,
+            ),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              child: Text('Login'),
+              onPressed: _login,
+            ),
+            SizedBox(height: 8.0),
+            TextButton(
+              child: Text('Register'),
+              onPressed: () async {
+                Navigator.pushNamed(context, '/register');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
